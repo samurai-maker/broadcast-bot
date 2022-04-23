44027cfadf9944e683d57daea8dca46fae89a3af
samurai-maker <84018991+samurai-maker@users.noreply.github.com>
2022-04-23 07:49:20 -0700

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,9 +1,12 @@
 class Presets(object):
     WELCOME_MESSAGE = """
-<b>Hello.. </b>{}
-<code>I'm an official telecaster bot
-maintained by</code> <b>{}</b>
-<code>Keep me unblocked to receive ma official updates.</code>
+<b>ഹായ് സുഹൃത്തുക്കളെ,
+നിങ്ങൾക്കു പറയാനുള്ള കഥകളും അനുഭവങ്ങളും ആഗ്രഹങ്ങളുമെല്ലാം എനിക്കു അയച്ചുതരാം.
+തിരഞ്ഞെടുക്കുന്നവ ചാനലിൽ പോസ്റ്റ് {}  ചെയ്യുന്നതാണ്.
+</b>
+ <i>
+Note : നിങ്ങൾക് പറയാനുള്ളത് 120 വേഡ്സിൽ കൊള്ളുന്ന ലേഖനമായോ വോയിസ്, ഓഡിയോ ആയോ അയച്ചു തരിക.
+</i>
     """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
